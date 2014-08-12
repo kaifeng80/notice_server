365b4cd13b7afce57d1cf656a56d1cef228a7929
King Lee <lijin@chukong-inc.com>
2014-08-12 15:52:13 +0800

    add schedule log

--- nosql/redis_schedule_wrapper.js
+++ nosql/redis_schedule_wrapper.js
@@ -3,6 +3,7 @@
  */
 var redis_pools = require("../nosql/redis_pools");
 var h_schedule_notice = 'h_schedule_notice';
+var h_schedule_notice_log = 'h_schedule_notice_log';
 
 var redis_schedule_wrapper = module.exports;
 
@@ -64,4 +65,34 @@ redis_schedule_wrapper.clear_schedule = function(cb){
             release();
         });
     });
+};
+
+//////////////////////////////////////////////////////////////////////////////////////
+redis_schedule_wrapper.add_schedule_log = function(type,schedule){
+    redis_pools.execute('pool_1',function(client, release){
+        var operator = {};
+        var __date = new Date();
+        operator.time = __date.toGMTString();
+        operator.type = type;
+        client.hset(h_schedule_notice_log,JSON.stringify(operator),JSON.stringify(schedule),function (err, reply){
+               if(err){
+                //  some thing log
+                    console.error(err);
+                }
+                release();
+            });
+       });
+    };
+
+redis_schedule_wrapper.get_all_schedule_log = function(cb){
+    redis_pools.execute('pool_1',function(client, release){
+            client.hgetall(h_schedule_notice_log,function (err, reply){
+                  if(err){
+                         //  some thing log
+                       console.error(err);
+                    }
+                   cb(reply);
+                    release();
+             });
+      });
 };
