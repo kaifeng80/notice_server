89f7a0528e5b0bb501de73b208d183d90786d7ef
King Lee <lijin@chukong-inc.com>
2015-03-12 11:07:59 +0800

    update config

--- app.js
+++ app.js
@@ -51,6 +51,8 @@ var announcement_230 = require('./routes/announcement_230');
 var announcement_230_000013 = require('./routes/announcement_230_000013');
 var announcement_230_000023 = require('./routes/announcement_230_000023');
 
+var announcement_240 = require('./routes/announcement_240');
+
 var rewardlist_seoul_200 = require('./routes/rewardlist_seoul_200');
 var rewardlist_seoul_200_000005 = require('./routes/rewardlist_seoul_200_000005');
 var rewardlist_seoul_200_000020 = require('./routes/rewardlist_seoul_200_000020');
@@ -140,6 +142,8 @@ app.use('/announcement_220_000442', announcement_220_000442);
 app.use('/announcement_230', announcement_230);
 app.use('/announcement_230_000013', announcement_230_000013);
 app.use('/announcement_230_000023', announcement_230_000023);
+
+app.use('/announcement_240', announcement_240);
 
 app.use('/rewardlist_seoul_200', rewardlist_seoul_200);
 app.use('/rewardlist_seoul_200_000005', rewardlist_seoul_200_000005);
