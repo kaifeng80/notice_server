e787f6897d6f26c50744a1b724d0619ca8daca88
King Lee <lijin@chukong-inc.com>
2015-03-16 15:01:22 +0800

    add config

---
+++ routes/reward_vs_weekly_230.js
@@ -0,0 +1,13 @@
+/**
+ * Created by King Lee on 15-1-15.
+ */
+var express = require('express');
+var router = express.Router();
+var util = require('../module/util');
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('reward_vs_weekly_230', { title: 'Express', date_desc: util.getWeekDesc() });
+});
+
+module.exports = router;
